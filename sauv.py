bc506a267a668a7a36e2cb5115546b5ba645d9ae
Jelared <57228122+Jelared@users.noreply.github.com>
2024-05-13 14:19:02 +0200

    Update sauv.py

--- sauv.py
+++ sauv.py
@@ -1,7 +1,22 @@
 def dechiffrage():
-    Sentence = "Jsiueolruseusnpueose"
-    # Boucle pour sélectionner alternativement une lettre de chaque phrase
+    Sentence = "Jsiueolruseusnpueoses"
+    # Calcul de la longueur et séparation en deux phrases
     longueur_phrase = len(Sentence)
-    print(longueur_phrase)
+    split_sentence = longueur_phrase//2
+    Sentence_A = Sentence[:split_sentence]
+    Sentence_B = Sentence[split_sentence:]
+    
+    # Boucle pour sélectionner alternativement une lettre de chaque phrase
+    Resultat = ""
+    for lettreA, lettreB in zip(Sentence_A, Sentence_B):
+        Resultat += lettreA + lettreB
+        
+    # Ajout d'une lettre si impaire
+    if longueur_phrase % 2 != 0:
+        Resultat += Sentence[-1]
+    
+    print(Sentence_A)
+    print(Sentence_B)
+    print(Resultat)
 
 dechiffrage()
