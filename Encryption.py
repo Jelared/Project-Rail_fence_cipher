6d0215b2d6fa188d1cef322ee7a3636045de57cb
Jelared <57228122+Jelared@users.noreply.github.com>
2024-05-14 13:31:08 +0200

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,33 +1,33 @@
-def chiffrage(Sentence):
+def chiffrage(Sentence_C):
 
-    resultat = ""
+    resultat_C = ""
 
     # Remplacement des espaces
-    Sentence_without_space = Sentence.replace(" ", "") 
+    Sentence_without_space = Sentence_C.replace(" ", "") 
 
     # Phrase une lettre sur deux
     Sentence_A = Sentence_without_space[::2] # en commençant à la première lettre
     Sentence_B = Sentence_without_space[1::2] # en commençant à la deuxième lettre
 
-    resultat = Sentence_A + Sentence_B
+    resultat_C = Sentence_A + Sentence_B
     
-    return resultat
+    return resultat_C
 
-def dechiffrage(Sentence):
+def dechiffrage(Sentence_D):
 
     # Calcul de la longueur et séparation en deux phrases
-    longueur_phrase = len(Sentence)
+    longueur_phrase = len(Sentence_D)
     split_sentence = longueur_phrase//2
     Sentence_A = Sentence[:split_sentence]
     Sentence_B = Sentence[split_sentence:]
     
     # Boucle pour sélectionner alternativement une lettre de chaque phrase
-    resultat = ""
+    resultat_D = ""
     for lettreA, lettreB in zip(Sentence_A, Sentence_B):
-        resultat += lettreA + lettreB
+        resultat_D += lettreA + lettreB
         
     # Ajout d'une lettre si impaire
     if longueur_phrase % 2 != 0:
-        resultat += Sentence[-1]
+        resultat_D += Sentence_D[-1]
     
-    return resultat
+    return resultat_D
