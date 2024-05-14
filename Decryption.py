d94bbcd30e4793523e75d658456b871302d6419c
Jelared <57228122+Jelared@users.noreply.github.com>
2024-05-14 13:32:00 +0200

    Update Decryption.py

--- Decryption.py
+++ Decryption.py
@@ -1,21 +1,18 @@
-def dechiffrage(Sentence):
+def dechiffrage(Sentence_D):
 
-    Sentence ="Jsiueolruseusnpueoses"
     # Calcul de la longueur et séparation en deux phrases
-    longueur_phrase = len(Sentence)
+    longueur_phrase = len(Sentence_D)
     split_sentence = longueur_phrase//2
     Sentence_A = Sentence[:split_sentence]
     Sentence_B = Sentence[split_sentence:]
     
     # Boucle pour sélectionner alternativement une lettre de chaque phrase
-    resultat = ""
+    resultat_D = ""
     for lettreA, lettreB in zip(Sentence_A, Sentence_B):
-        resultat += lettreA + lettreB
+        resultat_D += lettreA + lettreB
         
     # Ajout d'une lettre si impaire
     if longueur_phrase % 2 != 0:
-        resultat += Sentence[-1]
+        resultat_D += Sentence_D[-1]
     
-    return resultat
-
-    dechiffrage()
+    return resultat_D
