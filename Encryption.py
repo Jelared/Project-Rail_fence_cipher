32f7d56cc9e247b744d0dd0a8df19c05fb7cbc08
Jelared <57228122+Jelared@users.noreply.github.com>
2024-05-14 13:31:42 +0200

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -12,22 +12,3 @@ def chiffrage(Sentence_C):
     resultat_C = Sentence_A + Sentence_B
     
     return resultat_C
-
-def dechiffrage(Sentence_D):
-
-    # Calcul de la longueur et séparation en deux phrases
-    longueur_phrase = len(Sentence_D)
-    split_sentence = longueur_phrase//2
-    Sentence_A = Sentence[:split_sentence]
-    Sentence_B = Sentence[split_sentence:]
-    
-    # Boucle pour sélectionner alternativement une lettre de chaque phrase
-    resultat_D = ""
-    for lettreA, lettreB in zip(Sentence_A, Sentence_B):
-        resultat_D += lettreA + lettreB
-        
-    # Ajout d'une lettre si impaire
-    if longueur_phrase % 2 != 0:
-        resultat_D += Sentence_D[-1]
-    
-    return resultat_D
